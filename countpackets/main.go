446c622df69cc37aab0fa4223cb445944a2aeabc
Leonardo Di Donato <leodidonato@gmail.com>
2019-10-17 03:39:27 +0000

    new: count packets with eBPF
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ countpackets/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"syscall"
+	"time"
+	"unsafe"
+
+	bpflib "github.com/iovisor/gobpf/elf"
+	"github.com/skydive-project/skydive/common"
+	"github.com/vishvananda/netlink"
+)
+
+const howlong = 10 * time.Second
+const interval = 1 * time.Second
+
+func getBPF() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(dir, "pkts.o"), nil
+}
+
+func main() {
+	bpf, err := getBPF()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "unable to find `pkts.o` eBPF file")
+		os.Exit(1)
+	}
+
+	m := bpflib.NewModule(bpf)
+	if err := m.Load(nil); err != nil {
+		fmt.Fprintln(os.Stderr, "error loading pkts.o")
+		os.Exit(1)
+	}
+
+	countpacketsMap := m.Map("packets")
+	if countpacketsMap == nil {
+		fmt.Fprintln(os.Stderr, "unable to find `packets` eBPF map")
+		os.Exit(1)
+	}
+
+	countpacketsProgram := m.SocketFilter("socket/countpackets")
+	if countpacketsProgram == nil {
+		fmt.Fprintln(os.Stderr, "unable to find `countpackets` socket filter eBPF program")
+		os.Exit(1)
+	}
+
+	links, err := netlink.LinkList()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	for _, link := range links {
+		// open a socket in the network namespace for interface
+		rs, err := common.NewRawSocketInNs("/proc/1/ns/net", link.Attrs().Name, syscall.ETH_P_ALL)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := bpflib.AttachSocketFilter(countpacketsProgram, rs.GetFd()); err != nil {
+			fmt.Fprintln(os.Stderr, "unable to attach `countpackets` socket filter eBPF program")
+			os.Exit(1)
+		}
+	}
+
+	tick := time.NewTicker(interval)
+	quit := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-tick.C:
+				poll(m, countpacketsMap)
+			case <-quit:
+				fmt.Fprintln(os.Stdout, "quit")
+				return
+			}
+		}
+	}()
+
+	time.Sleep(howlong)
+	tick.Stop()
+	close(quit)
+	time.Sleep(time.Millisecond * 500)
+}
+
+func poll(bpf *bpflib.Module, m *bpflib.Map) {
+	var err error
+	var k, v, n uint64
+	still := true
+	for still {
+		still, err = bpf.LookupNextElement(m, unsafe.Pointer(&k), unsafe.Pointer(&n), unsafe.Pointer(&v))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			break
+		}
+
+		fmt.Println(k, v)
+		k = n
+	}
+	fmt.Println("-----")
+}
